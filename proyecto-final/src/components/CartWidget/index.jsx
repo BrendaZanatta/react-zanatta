55f2a5ba905f89285ce0ebdf2bb41b6ee396e253
Brenda Zanatta <brendazanatta1@gmail.com>
2022-04-21 15:32:35 -0300

    tercer commit

---
+++ proyecto-final/src/components/CartWidget/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './cart-widget.css';
+
+const CartWidget = () => {
+  return (
+    <div className='carrito'><img src="./imagenes/img1.jpg" alt="imagen no disponible" /></div>
+  )
+}
+
+export default CartWidget
--- proyecto-final/src/components/NavBar/NavBar.jsx
+++ proyecto-final/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NavBar.css';
-<link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet"></link>
+
 
 function NavBar(props) {
     return (
@@ -8,14 +8,14 @@ function NavBar(props) {
             <h1 className='logo'>Ocean Cakes</h1>
 
             <ul className='nav'>
-                <a href='#'>Inicio</a>
-                <a href='#'>Productos</a>
-                <a href='#'>Nosotras</a>
-                <a href='#'>Preguntas Frecuentes</a>
-                <a href='#'>Contacto</a>
+                <li><a href='#'>Inicio</a></li>
+                <li><a href='#'>Productos</a></li>
+                <li><a href='#'>Nosotras</a></li>
+                <li><a href='#'>Preguntas Frecuentes</a></li>
+                <li><a href='#'>Contacto</a></li>    
             </ul>
         </div>
     );
 }
 
-export default NavBar;
+export default NavBar;  
